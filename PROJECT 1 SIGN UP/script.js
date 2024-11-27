982334bf6edf97bcd1a84ff3e8c3c4e59a86cc89
Bilal Yousuf <bilaljan838@gmail.com>
2024-11-28 02:37:09 +0500

    JS.

--- PROJECT 1 SIGN UP/script.js
+++ PROJECT 1 SIGN UP/script.js
@@ -18,6 +18,18 @@ function showError(input, message){
     small.innerText = message;
 }
 
+// Function to update class for success
+function showSuccess(){
+    // Get the parent element of the input field (.form-control)
+    const formControl = input.parentElement;
+    // Override/replace the class - add success
+    formControl.className = 'form-control success';
+}
+// Function to check if email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 // Event Listeners
 // Create Event Listener Button
 form.addEventListener('submit', function(e){
@@ -30,6 +42,40 @@ form.addEventListener('submit', function(e){
     if(username.value === ''){
         // alert('Username is required');
         showError(username, 'Username is required');
+    }
+    else{
+        showError(username);
+        //console.log(username.value);
+    }
+
+    // Check if email is empty
+    if(email.value === ''){
+        // alert('Email is required');
+        showError(email, 'Email is required');
+    }
+    else{
+        showError(email);
+        //console.log(email.value);
+    }
+
+    // Check if password is empty
+    if(password.value === ''){
+        // alert('Password is required');
+        showError(password, 'Password is required');
+    }
+    else{
+        showError(password);
+        //console.log(password.value);
+    }
+
+    // Check if confirm-password is empty
+    if(confirmPassword.value === ''){
+        // alert('confirmPassword is required');
+        showError(confirmPassword, 'confirmPassword is required');
+    }
+    else{
+        showError(confirmPassword);
+        //console.log(confirmPassword.value);
     }
 });
 
